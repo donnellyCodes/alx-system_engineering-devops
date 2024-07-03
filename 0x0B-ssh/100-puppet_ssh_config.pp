4574c968ce3b0f745155f3d51ea09660998eda3d
donnelly19 <donamaitsa@gmail.com>
2024-07-03 03:38:29 -0700

    configuration with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# Use puppet to configure SSH file access without authentication
+
+exec {'/etc/ssh/ssh_config':
+  path    => '/bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentifyFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+}
