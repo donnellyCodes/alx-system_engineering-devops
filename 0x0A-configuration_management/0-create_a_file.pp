2d3496f2dae08a0752797b620ae930b6a08a16d8
donnelly19 <donamaitsa@gmail.com>
2024-07-08 23:22:49 -0700

    creating a file using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Creating a file using puppet
+
+file { '/tmp/school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
