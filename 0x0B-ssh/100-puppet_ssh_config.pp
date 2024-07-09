3c76563d7bbb538b9b46b265c0580c93b94d9ed8
donnelly19 <donamaitsa@gmail.com>
2024-07-09 13:34:38 -0700

    fixing error in puppet configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,5 @@
 # Use puppet to configure SSH file access without authentication
-file_line {'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-}
-
-file_line {'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentifyFile ~/.ssh/school',
+exec {'/etc/ssh/ssh_config':
+  path => 'bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentifyFile ~/.ssh/school" >> /etc/ssh/ssh_config',
 }
