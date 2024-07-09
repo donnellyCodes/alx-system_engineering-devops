862dcceaf405fd7f5f27dec0c4a60c05af722bc2
donnelly19 <donamaitsa@gmail.com>
2024-07-09 00:17:28 -0700

    using puppet to kill a process

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# Manifest that kills a process called killmenow
+
+exec { 'pkill killmenow':
+  path     => '/usr/bin/',
+  command  => 'pkill killmenow',
+  provider => shell
+  returns  => [0, 1]
+}
