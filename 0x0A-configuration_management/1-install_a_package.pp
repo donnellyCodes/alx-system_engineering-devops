565e4c118e6f099f84e59b4d8a3113cbfa3e99b4
donnelly19 <donamaitsa@gmail.com>
2024-07-08 23:53:44 -0700

    fixing error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,25 @@
 #Installing Flask using puppet
 
+#ensure python is present
+package { 'python3.8':
+  ensure => present,
+}
+
+#ensure pip is present
+package { 'python3-pip':
+  ensure => present,
+}
+
+#install flask
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+#Install Werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
