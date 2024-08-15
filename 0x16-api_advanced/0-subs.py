b58a5cff2f8b603e0692cb13349ea32d7c27d0e9
donnelly19 <donamaitsa@gmail.com>
2024-08-15 14:26:22 -0700

    recursive function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
-contains the function number of subscribers
-"""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Returns number of subscribers in a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User_Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json
-        subscribers = data['data']['subscribers']
-        return subscribers
+        data = response.json().get("data")
+        return data.get("subscribers", 0)
     else:
         return 0
